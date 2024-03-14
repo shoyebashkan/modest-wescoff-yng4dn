f9a17461ae81ae9208a035edeaaa1b4a8712d536
Shoyeb Ashkan <111368904+shoyebashkan@users.noreply.github.com>
2024-03-14 13:32:58 +0530

    Update ColumnSection.js
    making code a bit more user-friendly

--- src/component/ColumnSection.js
+++ src/component/ColumnSection.js
@@ -1,11 +1,37 @@
-const CoulmnSection = ({ data }) => {
+const CoulmnSection = ({ data, forks }) => {
   return (
-    <div>
-      <div className="col">{data.name}</div>
-      <div className="col">{data.language}</div>
-      <div className="col">{data.description}</div>
-      <div className="col">{data.size}</div>
-    </div>
+    <section>
+      <header>
+        <div className="col">Name</div>
+        <div className="col">Language</div>
+        <div className="col">Description</div>
+        <div className="col">Size</div>
+      </header>
+
+      {forks
+        ? data.map((data) => {
+            return (
+              <div key={data.id}>
+                <div className="col">{data.name}</div>
+                <div className="col">{data.language}</div>
+                <div className="col">{data.description}</div>
+                <div className="col">{data.size}</div>
+              </div>
+            );
+          })
+        : data
+            .filter((data) => !data.fork)
+            .map((data) => {
+              return (
+                <div key={data.id}>
+                  <div className="col">{data.name}</div>
+                  <div className="col">{data.language}</div>
+                  <div className="col">{data.description}</div>
+                  <div className="col">{data.size}</div>
+                </div>
+              );
+            })}
+    </section>
   );
 };
 
